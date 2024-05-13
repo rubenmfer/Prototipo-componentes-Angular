9b6bbeda348146828526fa384f266c5bfdc21350
EC2 Default User <ec2-user@ip-172-31-5-153.ec2.internal>
2024-05-13 16:49:10 +0000

    Primer commit

---
+++ myAppAngular/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { LocalStorageDataComponent } from './local-storage-data/local-storage-data.component';
+import { DeleteItemComponent } from './delete-item/delete-item.component';
+
+const routes: Routes = [
+  {
+    path:'', component:AddTaskComponent,pathMatch:'full'
+  },
+  
+  {
+    path:'my-tasks', component: LocalStorageDataComponent
+  },
+  {
+    path:'**', component:AddTaskComponent,pathMatch:'full'
+  },
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
